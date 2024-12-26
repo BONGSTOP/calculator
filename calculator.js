c2c7b0a94600aa4aefe79fe2c3ff0406b04dec6e
BONGSTOP <jacobferreras24@gmail.com>
2024-12-26 18:09:33 +0800

    refactor the code

--- calculator.js
+++ calculator.js
@@ -1,54 +1,65 @@
-function additionFunction(num1, num2) {
-  return num1 + num2;
-}
+class Calculator {
+  constructor(displayElement) {
+    this.display = displayElement;
+  }
 
-function subtractionFunction(num1, num2) {
-  return num1 - num2;
-}
+  additionFunction(num1, num2) {
+    return num1 + num2;
+  }
 
-function multiplicationFunction(num1, num2) {
-  return num1 * num2;
-}
+  subtractionFunction(num1, num2) {
+    return num1 - num2;
+  }
 
-function divisionFunction(num1, num2) {
-  return num1 / num2;
+  multiplicationFunction(num1, num2) {
+    return num1 * num2;
+  }
+
+  divisionFunction(num1, num2) {
+    return num1 / num2;
+  }
+
+  complexOperation(num1, num2, num3) {
+    return num1 + num2 * num3;
+  }
+
+  appendNumber(input) {
+    this.display.value += input;
+  }
+
+  clearDisplay() {
+    this.display.value = "";
+  }
+
+  deleteDigit() {
+    this.display.value = this.display.value.slice(0, -1);
+  }
+
+  calculate() {
+    const expression = this.display.value;
+    const numbers = expression.split(/[-+*\/]/);
+    const operator = expression.match(/[-+*\/]/);
+    const num1 = parseFloat(numbers[0]);
+    const num2 = parseFloat(numbers[1]);
+
+    switch (operator[0]) {
+      case "+":
+        this.display.value = this.additionFunction(num1, num2);
+        break;
+      case "-":
+        this.display.value = this.subtractionFunction(num1, num2);
+        break;
+      case "*":
+        this.display.value = this.multiplicationFunction(num1, num2);
+        break;
+      case "/":
+        this.display.value = this.divisionFunction(num1, num2);
+        break;
+      default:
+        this.display.value = "Error";
+    }
+  }
 }
 
 const display = document.querySelector("#display");
-
-const appendNumber = (input) => {
-  display.value += input;
-};
-
-const clearDisplay = () => {
-  display.value = "";
-};
-
-const deleteDigit = () => {
-  display.value = display.value.slice(0, -1);
-};
-
-function calculate() {
-  const expression = display.value;
-  const numbers = expression.split(/[-+*\/]/);
-  const operator = expression.match(/[-+*\/]/);
-  const num1 = parseFloat(numbers[0]);
-  const num2 = parseFloat(numbers[1]);
-
-  switch (operator[0]) {
-    case "+":
-      display.value = additionFunction(num1, num2);
-      break;
-    case "-":
-      display.value = subtractionFunction(num1, num2);
-      break;
-    case "*":
-      display.value = multiplicationFunction(num1, num2);
-      break;
-    case "/":
-      display.value = divisionFunction(num1, num2);
-      break;
-    default:
-      display.value = "Error";
-  }
-}
+const calculator = new Calculator(display);
